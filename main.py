84480395dd0a7582501e29488489a1aa0298617b
Austin Lyksett <alyksett1029@gmail.com>
2024-11-13 15:38:49 -0600

    cleanups

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from langs import languages, Language
 import random
+from tabulate import tabulate
 
 def get_random_langage() -> Language:
     l = len(languages)
@@ -7,41 +8,44 @@ def get_random_langage() -> Language:
     return languages[rand]
 
 def subselect_passage(lang: Language):
-    # text = lang.text
-    # words = text.split(" ")
-    # length =  len(words)
-    # min_length = length//3
-    # max_length = length//2
-    # start = random.randint(0, length-max_length)
-    # end = random.randint(min_length, max_length)
-    # d = words[start:end + start]
-    # return " ".join(d)
-    return lang.text
+    text = lang.text
+    words = text.split(" ")
+    length =  len(words)
+    min_length = length//3
+    max_length = length//2
+    start = random.randint(0, length-max_length)
+    end = random.randint(min_length, max_length)
+    d = words[start:end + start]
+    return " ".join(d)
 
-def get_input(target:str, text:str):
+def get_input(target:str):
     guesses = 0
     hints = 1
     guess = ""
-    
-    while(guess.lower() != target):
-        guess = input("What language is this: ")    
-        
-        if(guess == "hint"):
-            print(" " * 10 + f"Hint: {target[0:hints]}")
-            hints += 1
-            hints+=1
-            continue
-        if(guess.lower() != target and guess != "idk"):
-            guesses += 1
-            print(" " * 10 + "Incorrect")
-        if(guess == "idk"):
-            guesses = -1
-            print(target)
-            break
-        if(guess.lower() == target):
-            print(" " * 10 + "Correct")
-            break
-    return guesses, hints-1 
+
+    while True:
+        guess = input("What language is this: ")
+        match guess.lower():
+            case "help":
+                print("===========")
+                print("\n".join([l.language for l in languages]))
+                print("===========")
+                continue
+            case "hint":
+                print(" " * 10 + f"Hint: {target[0:hints]}")
+                hints += 1
+                continue
+            case "idk":
+                guesses = -1
+                print(target)
+                break
+            case _ if guess != target.lower():
+                guesses += 1
+                print(" " * 10 + "Incorrect")
+            case _ if guess == target.lower():
+                print(" " * 10 + "Correct")
+                break
+    return guesses, hints-1
 
 def try_window(languages: list[Language]) ->list[Language]:
     res = []
@@ -51,8 +55,8 @@ def try_window(languages: list[Language]) ->list[Language]:
         p = "\n".join([l.text[i:i+60] for i in range(0, len(l.text), 60)])
         print(p)
         print("-"*60)
-        guesses, hints = get_input(target=l.language.lower(), text=l.text)
-        if guesses > 0 or hints > 1:
+        guesses, hints = get_input(target=l.language.lower())
+        if guesses > 0 or hints > 0:
             res.append(l)
         if guesses > 3:
             long_res.append(l)
@@ -61,23 +65,26 @@ def try_window(languages: list[Language]) ->list[Language]:
 def main():
     langs: list[Language] = languages
     random.shuffle(languages)
-    scores = {(langs[i].language for i in langs) : 0}
+    scores = {l.language: 0 for l in langs}
+    
     n = len(langs)
-    k = 2
+    k = 4
     low = 0
     long_retries = []
     for i in range(n-k+1):
         subset = langs[low:i+k]
         retries = try_window(subset)
-        for r in retries:
-            scores[r.language] += 1
         while(retries):
             for r in retries:
                 scores[r.language] += 1
             random.shuffle(retries)
             retries = try_window(retries)
         low = i+k
-    
+    folded = [[language, score] for language, score in scores.items()]
+    folded = sorted(folded, key=lambda s:s[1], reverse=True)
+    print(tabulate(folded, headers=["Language", "Score"], colalign=["left"]))
+    print("\"Score\" is the number of attempts.")
+
 main()
 
 
